aa854c95e8e8f95dcdf761b1d3c72fab9f3b5bef
gbvsilva <graco.babeuf18@gmail.com>
2019-07-04 17:30:39 +0000

    Back to port 80

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Install Docker from https://docs.docker.com/install
 E.g. execute:
 
 - `docker pull gbvsilva/vid2mp3`
-- `docker run -dit -p 7831:7831 --name vid2mp3 --restart always -v [path_to_host_folder]:/usr/src/vid2mp3 gbvsilva/vid2mp3`
+- `docker run -dit -p 80:80 -p 443:443 --name vid2mp3 --restart always -v [path_to_host_folder]:/usr/src/vid2mp3 gbvsilva/vid2mp3`
 
 The files already contained in the image:
 
 - index.html
+- server.js
--- files/server.js
+++ files/server.js
@@ -1,17 +1,48 @@
 'use strict';
 
+// Modules
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const path = require('path');
 // Constants
-const PORT = 7831;
-const HOST = '0.0.0.0';
+const PORT = 80;
 
 // App
 const app = express();
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.get('/', (req, res) => {
-  console.log('Request accepted');
-  res.sendfile('index.html');
+	console.log('Request from ' + req.connection.remoteAddress);
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+var url = '';
+var html = null;
+
+app.post('/send', (req, res) => {
+	url = req.body.url;
+	console.log('Video link -> ' + url);	
+	run();
+	res.send(html);
+});
+
+const puppeteer = require('puppeteer');
+
+
+async function run() {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url, { waitUntil: "networkidle2" });
+	// hacky defensive move but I don't know a better way:
+	// wait a bit so that the browser finishes executing JavaScript
+	await page.waitFor(1 * 1000);
+	html = await page.content();
+	//fs.writeFileSync("index.html", html);
+	await browser.close();
+}
+
+app.listen(PORT, function() {
+	console.log('Server running on port ' + PORT);
+});
